b99ecbbe184443a5d98757ef3be6ad0eff3b7448
Saif-09 <saifmd238@gmail.com>
2023-10-06 18:35:10 +0530

    creating event listener in nodejs

---
+++ index.js
@@ -0,0 +1,33 @@
+import events from "events";
+
+class FitnessTracker extends events.EventEmitter {
+  constructor() {
+    super();
+    this.progress = 0;
+    this.goal = 1000;
+  }
+
+  addExercise(exercise) {
+    // Write code to update the progress and emit a 'goalReached' event when the goal is reached
+    this.progress += exercise.caloriesBurned;
+    if(this.progress>= this.goal){
+      this.emit("goalReached")
+    }
+  }
+}
+
+const Solution = () => {
+  // define  listener that sends a congratulatory message to the user upon reaching their fitness goal
+  const fitnessTracker = new FitnessTracker();
+  fitnessTracker.addListener("goalReached",()=>{
+    console.log('Congratulations! You have reached your fitness goal')
+  })
+
+  // simulate adding exercise
+  fitnessTracker.addExercise({ name: "Running", caloriesBurned: 500 });
+  fitnessTracker.addExercise({ name: "Weightlifting", caloriesBurned: 600 });
+};
+
+Solution();
+
+export { FitnessTracker, Solution };
